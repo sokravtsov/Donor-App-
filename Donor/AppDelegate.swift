e6153be02827b039fd8a0ff30939a737412210a9
sokravtsov <kravtsov@happimess.ru>
2017-04-14 00:32:00 +0300

    add Remote Push Notification via Firebase

--- Donor/AppDelegate.swift
+++ Donor/AppDelegate.swift
@@ -9,6 +9,9 @@
 import UIKit
 import GoogleMaps
 import IQKeyboardManagerSwift
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,31 +25,69 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         IQKeyboardManager.sharedManager().enable = true
         
+        FIRApp.configure()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]){ (granted, error) in }
+        application.registerForRemoteNotifications()
+        
+        
+        NotificationCenter
+            .default
+            .addObserver(self, selector: #selector(tokenRefreshNotificaiton),
+                         name: NSNotification.Name.firInstanceIDTokenRefresh, object: nil)
+        
+        UIApplication.shared.applicationIconBadgeNumber = 0
+
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    func tokenRefreshNotificaiton(notification: NSNotification) {
+        let refreshedToken = FIRInstanceID.instanceID().token()
+        print("InstanceID token: \(String(describing: refreshedToken))")
+        // Connect to FCM since connection may have failed when attempted before having a token.
+        connectToFcm()
     }
+    
+    func connectToFcm() {
+        FIRMessaging.messaging().connect { (error) in
+            if let error = error {
+                print("Unable to connect with FCM. \(String(describing: error))")
+            } else {
+                print("Connected to FCM.")
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        print("Message ID: \(userInfo["gcm.message_id"]!)")
+        
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register:", error)
+    }
+    
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        print("APNs device token: \(deviceTokenString)")
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        FIRMessaging.messaging().disconnect()
+        print("Disconnected from FCM.")
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
+    func applicationWillEnterForeground(_ application: UIApplication) {}
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+    func applicationDidBecomeActive(_ application: UIApplication) {}
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    func applicationWillTerminate(_ application: UIApplication) {}
 }
 
--- Podfile
+++ Podfile
@@ -4,4 +4,6 @@ target 'Donor' do
   pod 'GoogleMaps'
   pod 'GooglePlaces'
   pod 'IQKeyboardManagerSwift', '4.0.9'
+  pod 'Firebase/Core'
+  pod 'Firebase/Messaging'
 end
